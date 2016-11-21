18684827193e3040f886961998709d5a6b94964e
Zowie van Geest <zowie@zowievangeest.nl>
2016-11-21 17:45:56 +0100

    Added comment and structure opdracht2

--- Week 1/opdracht_2.py
+++ Week 1/opdracht_2.py
@@ -5,28 +5,38 @@ https://dodona.ugent.be/nl/exercises/975017137/
 """
 import math
 
-# input voor het aantal radialen
-input_radials = float(input('Voer hier het aantal radialen in: '))
-
 
 # functie voor het converteren van radial naar hoek, minuten en seconden
 def convert_radials(radials):
-    # variabelen voor graden hoek en pi
-    angle = int(180)
-    pi = math.pi
-    result = ((angle / pi) * radials)
+    result = single_radial(radials)
+    print_result(result)
 
+
+# input voor het aantal radialen
+def input_radials():
+    return float(input('Voer hier het aantal radialen in: '))
+
+
+def single_radial(rad):
+    return int(180) / math.pi * rad
+
+
+def print_result(result):
     # variabelen voor het converteren naar graden minuten en seconden
     degrees = int(result)
     minuts = (result - int(result)) * 60
-    minuts_int = int((result - int(result)) * 60)
-    seconds_int = int((minuts - int(minuts)) * 60)
+    seconds = int((minuts - int(minuts)) * 60)
 
     # printen van de uitkomsten
-    print(degrees)
-    print(minuts_int)
-    print(seconds_int)
+    print(int(degrees), "\n", int(minuts), "\n", int(seconds))
 
 
-# aanroepen van de functie
-convert_radials(input_radials)
+# main functie
+def main():
+    # aanroepen van de functie
+    input_radial = input_radials()
+    convert_radials(input_radial)
+
+# runtime methode
+if __name__ == '__main__':
+    main()
