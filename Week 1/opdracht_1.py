bf5545a44617b4bf371cbc8909c2b000bd0394bc
Zowie van Geest <zowie@zowievangeest.nl>
2016-11-21 17:45:28 +0100

    Added comments and structure

--- Week 1/opdracht_1.py
+++ Week 1/opdracht_1.py
@@ -10,23 +10,36 @@ input_days = int(input('Voer hier het aantal dagen in: '))
 
 # functie voor het converteren van tijd naar decimaal
 def convert_time_to_float(time):
-    default_time = time
-    (h, m, s) = default_time.split(':')
+    # splitten van de ingevoerde tijdsbestek
+    (h, m, s) = time.split(':')
+    # omzetten naar seconden
     result_time = int(h) * 3600 + int(m) * 60 + float(s)
+    # return van het totaal aantal seconden
     return result_time
 
 
 # functie voor het converteren van seconden naar dagen uren en minuten
 def convert_seconds(seconds):
+    # dagen, uren, minuten en seconden omzetten van totaal aantaal seconden
     days, seconds = divmod(seconds, 24 * 60 * 60)
     hours, seconds = divmod(seconds, 60 * 60)
     minutes, seconds = divmod(seconds, 60)
-    print(input_days, 'sol =', days, 'dagen,', hours, 'uren,', minutes, 'minuten en', seconds, 'seconden')
+
+    # uitprinten van dagen, uren, minuten en seconden
+    print(input_days, 'sol =', int(days), 'dagen,', int(hours), 'uren,', int(minutes), 'minuten en', int(seconds),
+          'seconden')
 
 
-# converteren van 24uurs notatie naar seconden
-sol_time_decimal = convert_time_to_float("24:39:35.244")
-sol_time_multiply = int(input_days * sol_time_decimal)
+# main methode
+def main():
+    # converteren van 24uurs notatie naar seconden
+    sol_time_decimal = convert_time_to_float("24:39:35.244")
+    sol_time_multiply = float(input_days * sol_time_decimal)
 
-# printen van seconden naar dagen uren minuten en seconden
-print(convert_seconds(sol_time_multiply))
+    # printen van seconden naar dagen uren minuten en seconden
+    convert_seconds(sol_time_multiply)
+
+
+# runtime methode
+if __name__ == '__main__':
+    main()
