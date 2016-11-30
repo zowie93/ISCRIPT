c0ae6e6e10831971a72961264e44d54e60cff931
Zowie van Geest <zowie@zowievangeest.nl>
2016-11-30 19:21:36 +0100

    Added opdracht 10 and 11

--- Week 2/medt_opdracht_10.py
+++ Week 2/medt_opdracht_10.py
@@ -3,3 +3,42 @@ Opdracht 10 - Palindroomzinnen
 
 https://dodona.ugent.be/nl/exercises/23570674/
 """
+
+
+def input_phrases_count():
+    return int(input("Aantal zinnen: "))
+
+
+def give_input_phrase():
+    return input("Zin: \n")
+
+
+def strip_non_alpha_chars(string):
+    return ''.join(ch.lower() for ch in string if ch.isalpha())
+
+
+def check_if_palindroom(string):
+    string = strip_non_alpha_chars(string)
+    return string == string[::-1]
+
+
+def input_phrases(count):
+    phrases = []
+
+    for i in range(0, count):
+        phrase = give_input_phrase()
+        phrases.append(phrase)
+
+    return phrases
+
+
+def main():
+    count = input_phrases_count()
+    phrases = input_phrases(count)
+
+    for phrase in phrases:
+        print("{}palindroom zin".format(['geen ', ''][check_if_palindroom(phrase)]))
+
+
+if __name__ == '__main__':
+    main()
--- Week 2/medt_opdracht_11.py
+++ Week 2/medt_opdracht_11.py
@@ -3,3 +3,98 @@ Opdracht 11 - Sterke wachtwoorden
 
 https://dodona.ugent.be/nl/exercises/417422714/
 """
+
+
+def input_passwords_count():
+    return int(input("Aantal wachtwoorden: "))
+
+
+def give_input_password():
+    return input("Wachtwoord: ")
+
+
+def input_passwords(count):
+    passwords = []
+
+    for i in range(0, count):
+        password = give_input_password()
+        passwords.append(password)
+
+    return passwords
+
+
+def check_password_length(password):
+    if len(password) >= 8:
+        return True
+    else:
+        return False
+
+
+def check_password_capital(password):
+    for character in password:
+        if character.isupper():
+            return True
+        else:
+            continue
+    return False
+
+
+def check_password_lower(password):
+    for character in password:
+        if character.islower():
+            return True
+        else:
+            continue
+    return False
+
+
+def check_password_digit(password):
+    for character in password:
+        if character.isdigit():
+            return True
+        else:
+            continue
+    return False
+
+
+def check_password_specialchar(password):
+    if set('[~!@#$%^&*()_+{}":;\']+$').intersection(password):
+        return True
+    else:
+        return False
+
+
+def check_strength(passwords):
+    for password in passwords:
+        strength = 0
+        if check_password_length(password):
+            strength += 1
+        if check_password_capital(password):
+            strength += 1
+        if check_password_lower(password):
+            strength += 1
+        if check_password_digit(password):
+            strength += 1
+        if check_password_specialchar(password):
+            strength += 1
+
+        print(password_strength(strength))
+
+
+def password_strength(strength):
+    if strength < 3:
+        return "Zwak"
+    elif strength < 5:
+        return "Matig"
+    else:
+        return "Sterk"
+
+
+def main():
+    count = input_passwords_count()
+    passwords = input_passwords(count)
+    check_strength(passwords)
+
+
+if __name__ == '__main__':
+    main()
