cd3d7ba5a3829781e33ef642437ad8e2efacccf6
Zowie van Geest <zowie@zowievangeest.nl>
2016-12-01 13:40:49 +0100

    Comments and opdracht12

--- Week 2/medt_opdracht_11.py
+++ Week 2/medt_opdracht_11.py
@@ -5,33 +5,45 @@ https://dodona.ugent.be/nl/exercises/417422714/
 """
 
 
+# functie aantal wachtwoorden invullen
 def input_passwords_count():
     return int(input("Aantal wachtwoorden: "))
 
 
+# functie invoere nwachtwoorden
 def give_input_password():
     return input("Wachtwoord: ")
 
 
+# functie invoeren wachtwoorden
 def input_passwords(count):
+    # wachtwoorden opslaan in array
     passwords = []
 
+    # loop voor controle aantal ingevulde wachtwoorden input types printen
     for i in range(0, count):
+        # print vraagstelling
         password = give_input_password()
+        # wachtwoord toevoegen aan de arraylist
         passwords.append(password)
 
+    # terugggeven van de wachtwoorden
     return passwords
 
 
+# check voor wachtwoord lengte
 def check_password_length(password):
+    # kijkt of het ingevoerde wachtwoord langer is dan 8 chars
     if len(password) >= 8:
         return True
     else:
         return False
 
 
+# check voor wachtwoord met hoofdletter
 def check_password_capital(password):
     for character in password:
+        # kijkt of het wachtwoord een uppercase bevat
         if character.isupper():
             return True
         else:
@@ -39,8 +51,10 @@ def check_password_capital(password):
     return False
 
 
+# check voor password met lowercase
 def check_password_lower(password):
     for character in password:
+        # kijkt of het wachtwoord een char heeft met lowercase
         if character.islower():
             return True
         else:
@@ -48,8 +62,10 @@ def check_password_lower(password):
     return False
 
 
+# check voor cijfer in wachtwoord
 def check_password_digit(password):
     for character in password:
+        # kijkt of het wachtwoord een cijfer bevat
         if character.isdigit():
             return True
         else:
@@ -57,16 +73,21 @@ def check_password_digit(password):
     return False
 
 
+# check speciale characters
 def check_password_specialchar(password):
+    # kijkt of het wachtwoord een speciale char heeft
     if set('[~!@#$%^&*()_+{}":;\']+$').intersection(password):
         return True
     else:
         return False
 
 
+# check voor sterkte wachtwoorden
 def check_strength(passwords):
     for password in passwords:
+        # sterkte begint bij nul
         strength = 0
+        # if statements ter controle of de wachtwoorden aan de eisen voldoen, zoja dan sterkte toevoegen
         if check_password_length(password):
             strength += 1
         if check_password_capital(password):
@@ -78,9 +99,11 @@ def check_strength(passwords):
         if check_password_specialchar(password):
             strength += 1
 
+        # uitprinten van sterkte wachtwoorden
         print(password_strength(strength))
 
 
+# wachtwoord returns voor de sterkste
 def password_strength(strength):
     if strength < 3:
         return "Zwak"
@@ -91,8 +114,11 @@ def password_strength(strength):
 
 
 def main():
+    # uitvoeren aantal wachtwoorden die je opwilt geven
     count = input_passwords_count()
+    # wachtwoord veld print x count van wachtwoorden
     passwords = input_passwords(count)
+    # sterkte wachtwoorden controleren
     check_strength(passwords)
 
 
--- Week 2/medt_opdracht_12.py
+++ Week 2/medt_opdracht_12.py
@@ -3,3 +3,33 @@ Opdracht 12 - Rorschahtest
 
 https://dodona.ugent.be/nl/exercises/695254392/
 """
+
+
+# functie voor omdraaien multiple line string input
+def reflect_input(strings):
+    # loop door de ingevulde strings
+    for string in strings:
+        # elke string apart printen
+        print(string, end="")
+        # omdraaien van eerdergeprint strings
+        for c in range(len(string) - 1, 0 - 1, -1):
+            print(string[c], end="")
+        print("\n", end="")
+
+
+def main():
+    # input is leeg in het begin
+    input_string = ' '
+    # arraylist van strings
+    strings = []
+    # wanneer de string niet leeg zijn print dan alle strings achterstevoren
+    while input_string != "":
+        input_string = input()
+        strings.append(input_string)
+
+    # omdraaien ingevoerde strings
+    reflect_input(strings)
+
+
+if __name__ == '__main__':
+    main()
